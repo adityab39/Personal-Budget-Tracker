aee250a6ff8f7a904fd2b67d43714d0101240329
adityab39 <adityabahulikar39@gmail.com>
2025-04-10 17:24:31 -0400

    Implement user existence check during account creation and improve success message

--- budget-tracker-backend/app/controllers/users_controller.rb
+++ budget-tracker-backend/app/controllers/users_controller.rb
@@ -5,6 +5,16 @@ class UsersController < ApplicationController
         email = params[:email]
         mobile = params[:mobile]
         password = BCrypt::Password.create(params[:password])
+        existing_user_sql = <<-SQL
+            SELECT * FROM users WHERE email = '#{email}' OR mobile = '#{mobile}';
+        SQL
+
+        existing_user = ActiveRecord::Base.connection.exec_query(existing_user_sql)
+
+        if existing_user.any?
+            render json: { error: "User already exists. Please log in." }, status: :conflict and return
+        end
+
         sql = <<-SQL
         INSERT INTO users (name, email, mobile, password_digest, created_at, updated_at)
         VALUES ('#{name}', '#{email}', '#{mobile}', '#{password}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
@@ -12,7 +22,7 @@ class UsersController < ApplicationController
         SQL
 
         result = ActiveRecord::Base.connection.exec_query(sql)
-        render json: { message: "User created manually", user: result.rows[0] }, status: :created
+        render json: { message: "Welcome, #{name}! Your account has been created.", user: result.rows[0] }, status: :created
     end
 end
     
